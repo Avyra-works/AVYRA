81be987b1ce434e53eaabfb51ed845c9a85becd5
Your Name <you@example.com>
2026-06-29 17:35:40 +0530

    Vercel Analytics fixed-3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,21 +3,18 @@ import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
 import './App.css';
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
     <>
-      {/* Your Website */}
-      <Analytics />
-    </>
-  );
-}
+      <MainLayout>
+        <Home />
+      </MainLayout>
 
-function App() {
-  return (
-    <MainLayout>
-      <Home />
-    </MainLayout>
+      <Analytics />
+      <SpeedInsights />
+    </>
   );
 }
 
