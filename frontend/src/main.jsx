32e72d4533a3bf9d20814ece497fdbed5fcfc132
Your Name <you@example.com>
2026-06-29 17:27:10 +0530

    Vercel Analytics fixed-2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,16 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { Analytics } from "@vercel/analytics/react";
-
-function App() {
-  return (
-    <>
-      {/* Your Website */}
-      <Analytics />
-    </>
-  );
-}
 
 
 createRoot(document.getElementById('root')).render(
